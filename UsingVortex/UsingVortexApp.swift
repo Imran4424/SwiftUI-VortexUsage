6313117156a2146ab19ac043022069b1b003fe57
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-03-02 16:22:04 +0600

    project init

---
+++ UsingVortex/UsingVortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingVortexApp.swift
+//  UsingVortex
+//
+//  Created by Shah Md Imran Hossain on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingVortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
